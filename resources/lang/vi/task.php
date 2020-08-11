5feba88f698455d197fc459759583f1bbf6ff132
anhphamdt98 <pham.hai.anh@sun-asterisk.com>
2020-08-11 11:01:23 +0700

    create layouts

---
+++ resources/lang/vi/task.php
@@ -0,0 +1,11 @@
+<?php 
+
+return [
+    'title' => 'Quản lý công việc',
+    'task_list' => 'Danh sách công việc',
+    'task' => 'Công việc',
+    'new_task' => 'Công việc',
+    'add_task' => 'Thêm công việc',
+    'current_task' => 'Công việc hiện tại',
+    'delete' => 'Xóa',
+];
